061875a72d949e3a8ac0a1e09a919b2fd67fbe9c
Jacek Czaja <jczaja@habana.ai>
2024-04-25 13:41:16 +0300

    rebased test_batch_* tests

--- server/tests/models/test_causal_lm.py
+++ server/tests/models/test_causal_lm.py
@@ -80,10 +80,12 @@ def test_batch_from_pb(default_pb_batch, default_causal_lm_batch):
     assert len(batch.input_ids) == size_of_padded_to_bucket 
 
     assert batch.input_ids[0][-2] == 4321
+    assert batch.input_ids[0][-3] == 1
     assert torch.all(batch.input_ids[0][:-3] == PAD_TOKEN)
     assert batch.input_ids[0][-1] == PAD_TOKEN
 
-    assert batch.attention_mask[0, -3:-2] == 1
+    assert batch.attention_mask[0, -2] == 1
+    assert batch.attention_mask[0, -3] == 1
     assert torch.all(batch.attention_mask[0, :-3] == 0)
 
     assert batch.past_key_values is None
@@ -226,11 +228,13 @@ def test_batch_concatenate(
     default_causal_lm, default_causal_lm_batch, default_multi_requests_causal_lm_batch
 ):
     next_batch_0 = default_causal_lm_batch
-    _, next_batch_0, _ = default_causal_lm.generate_token(next_batch_0)
-    _, next_batch_0, _ = default_causal_lm.generate_token(next_batch_0)
+    _, next_batch_0, _ = default_causal_lm.generate_token([next_batch_0])
+    _, next_batch_0, _ = default_causal_lm.generate_token([next_batch_0])
+    _, next_batch_0, _ = default_causal_lm.generate_token([next_batch_0])
 
     next_batch_1 = default_multi_requests_causal_lm_batch
-    _, next_batch_1, _ = default_causal_lm.generate_token(next_batch_1)
+    _, next_batch_1, _ = default_causal_lm.generate_token([next_batch_1])
+    _, next_batch_1, _ = default_causal_lm.generate_token([next_batch_1])
 
     # Clone past_key_values before concatenating to compare after,
     # because they are removed from the concatenated batches
@@ -243,113 +247,135 @@ def test_batch_concatenate(
 
     next_batch = CausalLMBatch.concatenate([next_batch_0, next_batch_1])
 
-    assert torch.equal(next_batch.all_input_ids[0], next_batch_0.all_input_ids[0])
-    assert torch.equal(next_batch.all_input_ids[1], next_batch_1.all_input_ids[0])
-    assert torch.equal(next_batch.all_input_ids[2], next_batch_1.all_input_ids[1])
+    assert torch.equal(next_batch.requests[0].all_input_ids, next_batch_0.requests[0].all_input_ids)
+    assert torch.equal(next_batch.requests[1].all_input_ids, next_batch_1.requests[0].all_input_ids)
+    assert torch.equal(next_batch.requests[2].all_input_ids, next_batch_1.requests[1].all_input_ids)
+
 
     assert torch.all(
-        next_batch.attention_mask[0, : -next_batch.padding_right_offset] == 1
+            next_batch.attention_mask[0:2, -next_batch.right_padding - 3: -next_batch.right_padding] == 1
     )
     assert torch.all(
-        next_batch.attention_mask[1:, 1 : -next_batch.padding_right_offset] == 1
+            next_batch.attention_mask[2, -next_batch.right_padding - 4: -next_batch.right_padding] == 1
     )
-    assert torch.all(next_batch.attention_mask[1:, 3:] == 0)
+    assert torch.all(
+            next_batch.attention_mask[3, -next_batch.right_padding - 3: -next_batch.right_padding] == 1
+    )
 
+    assert torch.all(
+            next_batch.attention_mask[0:2, :-next_batch.right_padding-3] == 0)
+    assert torch.all(
+        next_batch.attention_mask[2, :-next_batch.right_padding-4] == 0)
+    assert torch.all(
+        next_batch.attention_mask[3, :-next_batch.right_padding-3] == 0)
+    
     assert next_batch.batch_id == 0
-    assert next_batch.input_ids[0, 0] == 12355
-    assert torch.all(next_batch.input_ids[1:] == 292)
+    assert next_batch.input_ids[0,-next_batch.right_padding - 3] == 1
+    assert next_batch.input_ids[0,-next_batch.right_padding - 2] == 4321
+    assert next_batch.input_ids[0,-next_batch.right_padding - 1] == 292
 
-    assert next_batch.input_lengths == [3, 2, 2]
-    assert next_batch.max_input_length == 3
+    assert next_batch.max_input_length == 129
+
+    assert torch.all(next_batch.input_ids[0,-next_batch.right_padding:] == PAD_TOKEN)
+    assert torch.all(next_batch.input_ids[1,-next_batch.right_padding:] == PAD_TOKEN)
+    assert torch.all(next_batch.input_ids[2,-next_batch.right_padding:] == PAD_TOKEN)
+    assert torch.all(next_batch.input_ids[3,-next_batch.right_padding:] == PAD_TOKEN)
+
+    assert next_batch.input_length == PAD_SEQUENCE_TO_MULTIPLE_OF +1
+    assert next_batch.max_input_length == PAD_SEQUENCE_TO_MULTIPLE_OF +1
 
     assert next_batch.requests[0] == next_batch_0.requests[0]
     assert next_batch.requests[1:] == next_batch_1.requests
 
-    assert next_batch.next_token_choosers[0] == next_batch_0.next_token_choosers[0]
-    assert next_batch.next_token_choosers[1:] == next_batch_1.next_token_choosers
-
-    assert next_batch.stopping_criterias[0] == next_batch_0.stopping_criterias[0]
-    assert next_batch.stopping_criterias[1:] == next_batch_1.stopping_criterias
+    assert next_batch.requests[0].stopping_criteria == next_batch_0.requests[0].stopping_criteria
+    assert next_batch.requests[1].stopping_criteria == next_batch_1.requests[0].stopping_criteria
+    assert next_batch.requests[2].stopping_criteria == next_batch_1.requests[1].stopping_criteria
 
     assert next_batch.past_key_values is not None
-    assert all([p[0].shape == (3, 12, 2, 64) for p in next_batch.past_key_values])
-    assert all([p[1].shape == (3, 12, 2, 64) for p in next_batch.past_key_values])
+      
+    assert all([p[0].shape == (8, 32, 2048, 128) for p in next_batch.past_key_values])
+    assert all([p[1].shape == (8, 32, 2048, 128) for p in next_batch.past_key_values])
 
-    for i, past in enumerate(next_batch.past_key_values):
-        assert torch.equal(next_batch_0_past_key_values[i][0][0, :, -2:], past[0][0])
+    assert next_batch.past_key_values is not None
+
+    for i, past in enumerate(next_batch.past_key_values):       
+        assert torch.equal(next_batch_0_past_key_values[i][0][0, 0,0:128], past[0][0][0][1:129])
         assert torch.equal(
-            next_batch_1_past_key_values[i][0][:, :, -1:], past[0][1:, :, -1:, :]
+            next_batch_1_past_key_values[i][0][:, :, 0:1][0], past[0][1:, :, 1 :2, :][0]
         )
 
-        assert torch.equal(next_batch_0_past_key_values[i][1][0, :, -2:], past[1][0])
+        assert torch.equal(next_batch_0_past_key_values[i][1][0, 0,0:128], past[1][0][0][1:129])
         assert torch.equal(
-            next_batch_1_past_key_values[i][1][:, :, -1:], past[1][1:, :, -1:, :]
+            next_batch_1_past_key_values[i][1][:, :, 0:1][0], past[1][1:, :, 1 :2, :][0]
         )
+
+    generations, next_batch, _ = default_causal_lm.generate_token([next_batch])
 
     for _ in range(
-        default_multi_requests_causal_lm_batch.stopping_criterias[1].max_new_tokens - 2
+        default_multi_requests_causal_lm_batch.requests[1].stopping_criteria.max_new_tokens - 2
     ):
-        generations, next_batch, _ = default_causal_lm.generate_token(next_batch)
+        generations, next_batch, _ = default_causal_lm.generate_token([next_batch])
         assert len(generations) == len(next_batch)
 
-    generations, next_batch, _ = default_causal_lm.generate_token(next_batch)
+    generations, next_batch, _ = default_causal_lm.generate_token([next_batch])
     assert next_batch is not None
 
     assert len(generations) == 3
-    assert generations[2].generated_text.text == ".java:784)"
+    assert generations[2].generated_text.text == "ing the effect of a"
+
     assert (
         generations[2].request_id
-        == default_multi_requests_causal_lm_batch.requests[1].id
+        == default_multi_requests_causal_lm_batch.requests[1].data.id
     )
     assert (
         generations[2].generated_text.generated_tokens
-        == default_multi_requests_causal_lm_batch.stopping_criterias[1].max_new_tokens
+        == default_multi_requests_causal_lm_batch.requests[1].stopping_criteria.max_new_tokens
     )
 
     next_batch = next_batch.filter(
-        [next_batch.requests[0].id, next_batch.requests[1].id]
+        [next_batch.requests[0].data.id, next_batch.requests[1].data.id]
     )
 
     for _ in range(
-        default_causal_lm_batch.stopping_criterias[0].max_new_tokens
-        - default_multi_requests_causal_lm_batch.stopping_criterias[1].max_new_tokens
+        default_causal_lm_batch.requests[0].stopping_criteria.max_new_tokens
+        - default_multi_requests_causal_lm_batch.requests[1].stopping_criteria.max_new_tokens
         - 2
     ):
-        generations, next_batch, _ = default_causal_lm.generate_token(next_batch)
+        generations, next_batch, _ = default_causal_lm.generate_token([next_batch])
         assert len(generations) == len(next_batch)
 
-    generations, next_batch, _ = default_causal_lm.generate_token(next_batch)
+    generations, next_batch, _ = default_causal_lm.generate_token([next_batch])
     assert next_batch is not None
 
     assert len(generations) == 2
-    assert generations[0].generated_text.text == ".java:784) at net.minecraft."
-    assert generations[0].request_id == default_causal_lm_batch.requests[0].id
+    assert generations[0].generated_text.text == "ing the effect of a new method for the detection"
+    assert generations[0].request_id == default_causal_lm_batch.requests[0].data.id
     assert (
         generations[0].generated_text.generated_tokens
-        == default_causal_lm_batch.stopping_criterias[0].max_new_tokens
+        == default_causal_lm_batch.requests[0].stopping_criteria.max_new_tokens
     )
 
-    next_batch = next_batch.filter([next_batch.requests[1].id])
+    next_batch = next_batch.filter([next_batch.requests[1].data.id])
 
     for _ in range(
-        default_multi_requests_causal_lm_batch.stopping_criterias[0].max_new_tokens
-        - default_causal_lm_batch.stopping_criterias[0].max_new_tokens
-        - default_multi_requests_causal_lm_batch.stopping_criterias[1].max_new_tokens
+        default_multi_requests_causal_lm_batch.requests[0].stopping_criteria.max_new_tokens
+        - default_causal_lm_batch.requests[0].stopping_criteria.max_new_tokens
+        - default_multi_requests_causal_lm_batch.requests[1].stopping_criteria.max_new_tokens
         - 4
     ):
-        generations, next_batch, _ = default_causal_lm.generate_token(next_batch)
+        generations, next_batch, _ = default_causal_lm.generate_token([next_batch])
         assert len(generations) == len(next_batch)
 
-    generations, next_batch, _ = default_causal_lm.generate_token(next_batch)
+    generations, next_batch, _ = default_causal_lm.generate_token([next_batch])
     assert next_batch is None
 
     assert len(generations) == 1
-    assert generations[0].generated_text.text == ".java:784) at net.minecraft."
+    assert generations[0].generated_text.text == "ing the effect of a new method for the detection"
     assert (
         generations[0].request_id
-        == default_multi_requests_causal_lm_batch.requests[0].id
+        == default_multi_requests_causal_lm_batch.requests[0].data.id
     )
     assert (
         generations[0].generated_text.generated_tokens
-        == default_multi_requests_causal_lm_batch.stopping_criterias[0].max_new_tokens
+        == default_multi_requests_causal_lm_batch.requests[0].stopping_criteria.max_new_tokens
     )
